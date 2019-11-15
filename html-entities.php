143ed60b6f62740583729e509c903235bb2efd72
tiffany <tiffanyphilippi@hotmail.com>
2019-11-14 22:12:14 -0300

    & -> começo da lógica

--- html-entities.php
+++ html-entities.php
@@ -1,3 +1,332 @@
 <?php
 
+    $array_from_to =  array('\'' => '&apos;',
+  '<' => '&lt;',
+  '>' => '&gt;',
+  ' ' => '&nbsp;',
+  '¡' => '&iexcl;',
+  '¢' => '&cent;',
+  '£' => '&pound;',
+  '¤' => '&curren;',
+  '¥' => '&yen;',
+  '¦' => '&brvbar;',
+  '§' => '&sect;',
+  '¨' => '&uml;',
+  '©' => '&copy;',
+  'ª' => '&ordf;',
+  '«' => '&laquo;',
+  '¬' => '&not;',
+  '®' => '&reg;',
+  '¯' => '&macr;',
+  '°' => '&deg;',
+  '±' => '&plusmn;',
+  '²' => '&sup2;',
+  '³' => '&sup3;',
+  '´' => '&acute;',
+  'µ' => '&micro;',
+  '¶' => '&para;',
+  '·' => '&middot;',
+  '¸' => '&cedil;',
+  '¹' => '&sup1;',
+  'º' => '&ordm;',
+  '»' => '&raquo;',
+  '¼' => '&frac14;',
+  '½' => '&frac12;',
+  '¾' => '&frac34;',
+  '¿' => '&iquest;',
+  'À' => '&Agrave;',
+  'Á' => '&Aacute;',
+  'Â' => '&Acirc;',
+  'Ã' => '&Atilde;',
+  'Ä' => '&Auml;',
+  'Å' => '&Aring;',
+  'Æ' => '&AElig;',
+  'Ç' => '&Ccedil;',
+  'È' => '&Egrave;',
+  'É' => '&Eacute;',
+  'Ê' => '&Ecirc;',
+  'Ë' => '&Euml;',
+  'Ì' => '&Igrave;',
+  'Í' => '&Iacute;',
+  'Î' => '&Icirc;',
+  'Ï' => '&Iuml;',
+  'Ð' => '&ETH;',
+  'Ñ' => '&Ntilde;',
+  'Ò' => '&Ograve;',
+  'Ó' => '&Oacute;',
+  'Ô' => '&Ocirc;',
+  'Õ' => '&Otilde;',
+  'Ö' => '&Ouml;',
+  '×' => '&times;',
+  'Ø' => '&Oslash;',
+  'Ù' => '&Ugrave;',
+  'Ú' => '&Uacute;',
+  'Û' => '&Ucirc;',
+  'Ü' => '&Uuml;',
+  'Ý' => '&Yacute;',
+  'Þ' => '&THORN;',
+  'ß' => '&szlig;',
+  'à' => '&agrave;',
+  'á' => '&aacute;',
+   'â' => '&acirc;',
+  'ã' => '&atilde;',
+  'ä' => '&auml;',
+  'å' => '&aring;',
+  'æ' => '&aelig;',
+  'ç' => '&ccedil;',
+  'è' => '&egrave;',
+  'é' => '&eacute;',
+  'ê' => '&ecirc;',
+  'ë' => '&euml;',
+  'ì' => '&igrave;',
+  'í' => '&iacute;',
+  'î' => '&icirc;',
+  'ï' => '&iuml;',
+  'ð' => '&eth;',
+  'ñ' => '&ntilde;',
+  'ò' => '&ograve;',
+  'ó' => '&oacute;',
+  'ô' => '&ocirc;',
+  'õ' => '&otilde;',
+  'ö' => '&ouml;',
+  '÷' => '&divide;',
+  'ø' => '&oslash;',
+  'ù' => '&ugrave;',
+  'ú' => '&uacute;',
+  'û' => '&ucirc;',
+  'ü' => '&uuml;',
+  'ý' => '&yacute;',
+  'þ' => '&thorn;',
+  'ÿ' => '&yuml;',
+  'Œ' => '&OElig;',
+  'œ' => '&oelig;',
+  'Š' => '&Scaron;',
+  'š' => '&scaron;',
+  'Ÿ' => '&Yuml;',
+  'ƒ' => '&fnof;',
+  'ˆ' => '&circ;',
+  '˜' => '&tilde;',
+  'Α' => '&Alpha;',
+  'Β' => '&Beta;',
+  'Γ' => '&Gamma;',
+  'Δ' => '&Delta;',
+  'Ε' => '&Epsilon;',
+  'Ζ' => '&Zeta;',
+  'Η' => '&Eta;',
+  'Θ' => '&Theta;',
+  'Ι' => '&Iota;',
+  'Κ' => '&Kappa;',
+  'Λ' => '&Lambda;',
+  'Μ' => '&Mu;',
+  'Ν' => '&Nu;',
+  'Ξ' => '&Xi;',
+  'Ο' => '&Omicron;',
+  'Π' => '&Pi;',
+  'Ρ' => '&Rho;',
+  'Σ' => '&Sigma;',
+  'Τ' => '&Tau;',
+  'Υ' => '&Upsilon;',
+  'Φ' => '&Phi;',
+  'Χ' => '&Chi;',
+  'Ψ' => '&Psi;',
+  'Ω' => '&Omega;',
+  'α' => '&alpha;',
+  'β' => '&beta;',
+  'γ' => '&gamma;',
+  'δ' => '&delta;',
+  'ε' => '&epsilon;',
+  'ζ' => '&zeta;',
+  'η' => '&eta;',
+  'θ' => '&theta;',
+  'ι' => '&iota;',
+  'κ' => '&kappa;',
+  'λ' => '&lambda;',
+  'μ' => '&mu;',
+  'ν' => '&nu;',
+  'ξ' => '&xi;',
+  'ο' => '&omicron;',
+  'π' => '&pi;',
+  'ρ' => '&rho;',
+  'ς' => '&sigmaf;',
+  'σ' => '&sigma;',
+  'τ' => '&tau;',
+  'υ' => '&upsilon;',
+  'φ' => '&phi;',
+  'χ' => '&chi;',
+  'ψ' => '&psi;',
+  'ω' => '&omega;',
+  'ϑ' => '&thetasym;',
+  'ϒ' => '&Upsih;',
+  'ϖ' => '&piv;',
+  '–' => '&ndash;',
+  '—' => '&mdash;',
+  '‘' => '&lsquo;',
+  '’' => '&rsquo;',
+  '‚' => '&sbquo;',
+  '“' => '&ldquo;',
+  '”' => '&rdquo;',
+  '„' => '&bdquo;',
+  '†' => '&dagger;',
+  '‡' => '&Dagger;',
+  '•' => '&bull;',
+  '…' => '&hellip;',
+  '‰' => '&permil;',
+  '′' => '&prime;',
+  '″' => '&Prime;',
+  '‹' => '&lsaquo;',
+  '›' => '&rsaquo;',
+  '‾' => '&oline;',
+  '⁄' => '&frasl;',
+  '€' => '&euro;',
+  'ℑ' => '&image;',
+  '℘' => '&weierp;',
+  'ℜ' => '&real;',
+  '™' => '&trade;',
+  'ℵ' => '&alefsym;',
+  '←' => '&larr;',
+  '↑' => '&uarr;',
+  '→' => '&rarr;',
+  '↓' => '&darr;',
+  '↔' => '&harr;',
+  '↵' => '&crarr;',
+  '⇐' => '&lArr;',
+  '⇑' => '&UArr;',
+  '⇒' => '&rArr;',
+  '⇓' => '&dArr;',
+  '⇔' => '&hArr;',
+  '∀' => '&forall;',
+  '∂' => '&part;',
+  '∃' => '&exist;',
+  '∅' => '&empty;',
+  '∇' => '&nabla;',
+  '∈' => '&isin;',
+  '∉' => '&notin;',
+  '∋' => '&ni;',
+  '∏' => '&prod;',
+  '∑' => '&sum;',
+  '−' => '&minus;',
+  '∗' => '&lowast;',
+  '√' => '&radic;',
+  '∝' => '&prop;',
+  '∞' => '&infin;',
+  '∠' => '&ang;',
+  '∧' => '&and;',
+  '∨' => '&or;',
+  '∩' => '&cap;',
+  '∪' => '&cup;',
+  '∫' => '&int;',
+  '∴' => '&there4;',
+  '∼' => '&sim;',
+  '≅' => '&cong;',
+  '≈' => '&asymp;',
+  '≠' => '&ne;',
+  '≡' => '&equiv;',
+  '≤' => '&le;',
+  '≥' => '&ge;',
+  '⊂' => '&sub;',
+  '⊃' => '&sup;',
+  '⊄' => '&nsub;',
+  '⊆' => '&sube;',
+  '⊇' => '&supe;',
+  '⊕' => '&oplus;',
+   '⊗' => '&otimes;',
+  '⊥' => '&perp;',
+  '⋅' => '&sdot;',
+  '⌈' => '&lceil;',
+  '⌉' => '&rceil;',
+  '⌊' => '&lfloor;',
+  '⌋' => '&rfloor;',
+  '⟨' => '&lang;',
+  '⟩' => '&rang;',
+  '◊' => '&loz;',
+  '♠' => '&spades;',
+  '♣' => '&clubs;',
+  '♥' => '&hearts;',
+  '♦' => '&diams;');
+
+  // print_r($array_from_to);
+
+  $origem = "a&lt;&nbsp;&gt;";
+  //converter string pra array:
+  $array = str_split($origem);
+  $pattern = '/&([a-zA-Z0-9]+;)/m';
+  //$chaves será um array apenas dos caracteres para conversão
+  $chaves = array_keys($array_from_to);
+
+  $i = 0;
+  $estado = 0;
+  $destino = '';
+
+  print_r($array);
+  // print_r($chaves);
+
+      if($estado == 0){
+        
+        for($i; $i < sizeof($array); $i++) {
+
+          if($array[$i] != '&'){
+
+            if ($array[$i] == ';') {
+              $estado = 0;
+              $temp .= $array[$i];
+            } else {
+              if ($estado == 0) {
+                $destino .= $array[$i];
+              } else {
+                $temp .= $array[$i];
+              }
+            }
+          } else {
+            //entra aqui SE FOR UM &
+            $temp = $array[$i];
+            $estado++;
+          }
+
+        }
+          if($estado == 1){
+            //VERIFICA ATÉ A POSIÇÃO 7
+            
+            echo "\n\nECHO TEMP0\n" . $temp ."\n\n";
+            $temp .= $array[2];
+            echo "\n\nECHO TEMP2\n" . $temp ."\n\n";
+            $temp .= $array[3];
+            echo "\n\nECHO TEMP3\n" . $temp ."\n\n";
+            $temp .= $array[4];
+            echo "\n\nECHO TEMP4\n" . $temp ."\n\n";
+            $temp .= $array[5];
+            echo "\n\nECHO TEMP5\n" . $temp ."\n\n";
+            $temp .= $array[6];
+            echo "\n\nECHO TEMP6\n" . $temp ."\n\n";
+            $temp .= $array[7];
+            echo "\n\nECHO TEMP7\n" . $temp ."\n\n";
+
+            if($array[$i] == '&'){
+              // CONCATENAR TODOS OS CARACTERES ATÉ O MOMENTO E REINICIAR PRA ESTADO 0;
+              $destino .= $temp;
+            }
+
+            if($array[$i] == ';'){
+
+              if(str_replace(array_values($array_from_to), array_keys($array_from_to), $temp)){
+
+                $charDecrypt = str_replace(array_values($array_from_to), array_keys($array_from_to), $temp);
+
+                $destino .= $charDecrypt;
+
+              } else {
+                $destino .= $temp;
+                $estado = 0;
+              }
+
+            }
+      
+            // $destino .= $charDecrypt;
+            
+            $estado=0;
+            }
+          }
+  }
+  // echo $destino;
+  echo '{"resultado": "' .  $destino .'"}';
+
 ?>
