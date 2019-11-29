f47857add5157b1342c3b17fdd46357ee580ff96
igorBarros <44558153+igorBarros@users.noreply.github.com>
2019-11-29 20:15:39 -0300

    funcionando formulario

--- salvarbanco.php
+++ salvarbanco.php
@@ -4,21 +4,15 @@ include 'conecta_sql.php';
 
 // Para salvar contato do usuario
 
-if(isset($_REQUEST["salvar"])){
-
-    if($_REQUEST["salvar"] == "dados"){
+  
         $nome = $_REQUEST['nome'];
         $email = $_REQUEST['email'];
         $telefone = $_REQUEST['telefone'];
         $assunto = $_REQUEST['texto'];
 
-      
-      $sql = "INSERT INTO `usuario`(`nome`, `email`, `telefone`, `assunto`)
+        $sql = "INSERT INTO `usuario`(`nome`, `email`, `telefone`, `assunto`)
         VALUES ('".$nome."', '".$email."', '".$telefone."', '".$assunto."')";
         $stmt = $dbh->prepare($sql);
         $stmt->execute();
-        $rows = $stmt->fetchAll();
-        $num_reg = count($rows);
-    }
-}
+
 ?>
